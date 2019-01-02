fcd3ae040f84378b27847dad044644ca8d78e020
John Wolfe <dr.joewolfe@gmail.com>
2019-01-02 20:58:28 +0530

    Enhanced guide type with more logger methods

--- src/main/java/org/jwolfe/quetzal/library/trips/Guide.java
+++ src/main/java/org/jwolfe/quetzal/library/trips/Guide.java
@@ -1,5 +1,8 @@
 package org.jwolfe.quetzal.library.trips;
 
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Guide {
     public static void heading(String heading) {
         System.out.println(heading);
@@ -20,5 +23,37 @@ public class Guide {
 
     public static void log(String message) {
         System.out.println("\t- " + message);
+    }
+
+    public static <T> void log(Stream<T> stream) {
+        var elements = stream.collect(Collectors.toList());
+        System.out.print("\t");
+        for(var item : elements) {
+            System.out.print(item + "\t");
+        }
+
+        System.out.println();
+    }
+
+    public static <T> void log(Stream<T> stream, int count, String separator) {
+        var elements = stream
+                        .limit(count)
+                        .collect(Collectors.toList());
+        System.out.print("\t");
+        for(var item : elements) {
+            System.out.print(item + separator + " ");
+        }
+
+        System.out.println();
+    }
+
+    public static <T> void log(String title, Stream<T> stream) {
+        System.out.print("\t- " + title);
+        log(stream);
+    }
+
+    public static <T> void log(String title, Stream<T> stream, int count, String separator) {
+        System.out.print("\t- " + title);
+        log(stream, count, separator);
     }
 }
