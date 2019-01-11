6fd3d2cf1b3e374a4ae4172f369ba46e309ce4df
John Wolfe <dr.joewolfe@gmail.com>
2019-01-11 18:25:14 +0530

    +1 to backtracking-algorithms (longest path in a matrix with hurdles)

--- src/main/java/org/jwolfe/quetzal/library/general/Coordinate.java
+++ src/main/java/org/jwolfe/quetzal/library/general/Coordinate.java
@@ -1,5 +1,7 @@
 package org.jwolfe.quetzal.library.general;
 
+import java.util.Objects;
+
 public class Coordinate {
 
 	public int getX() {
@@ -26,22 +28,61 @@ public class Coordinate {
 		this.x = x;
 		this.y = y;
 	}
-	
+
 	public void set(Coordinate newCoordinate) {
-		if(newCoordinate == null) {
+		if (newCoordinate == null) {
 			return;
 		}
-		
+
 		this.x = newCoordinate.x;
 		this.y = newCoordinate.y;
 	}
-	
+
 	public void increment(Coordinate deltaCoordinate) {
-		if(deltaCoordinate == null) {
+		if (deltaCoordinate == null) {
 			return;
 		}
-		
+
 		this.x += deltaCoordinate.x;
 		this.y += deltaCoordinate.y;
+	}
+
+	public Coordinate buildNew(Coordinate deltaCoordinate) {
+		if (deltaCoordinate == null) {
+			return null;
+		}
+
+		return new Coordinate(this.x + deltaCoordinate.x, this.y + deltaCoordinate.y);
+	}
+
+	public boolean isWithinBounds(int top, int left, int bottom, int right) {
+		if (x < top || y < left
+				|| x > bottom || y > right) {
+			return false;
+		}
+
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Coordinate{" +
+				"x=" + x +
+				", y=" + y +
+				'}';
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Coordinate that = (Coordinate) o;
+		return x == that.x &&
+				y == that.y;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(x, y);
 	}
 }
